fc5d84bb834c04c62a22517fc5a9fb1e82b8fb02
jayanthi08 <54354123+jayanthi08@users.noreply.github.com>
2026-07-30 15:20:33 +0530

    Create solution.sql

---
+++ solution.sql
@@ -0,0 +1,56 @@
+-- ===========================================
+-- Library Management System
+-- Name:
+-- Register Number:
+-- ===========================================
+
+-- Task 1
+-- Create Library table
+
+-- Columns
+-- BookID INTEGER PRIMARY KEY
+-- BookTitle TEXT
+-- Author TEXT
+-- Category TEXT
+-- Price REAL
+
+
+
+-- Task 2
+-- Insert the following records
+
+-- (101,'Database System Concepts','Silberschatz','Database',750)
+-- (102,'Programming in C','Balagurusamy','Programming',550)
+-- (103,'Operating System','Galvin','System',680)
+-- (104,'Data Mining','Han','Database',820)
+-- (105,'Python Programming','Reema Thareja','Programming',600)
+
+
+
+-- Task 3
+-- Display all books
+
+
+
+-- Task 4
+-- Display Database books
+
+
+
+-- Task 5
+-- Display books having price >600
+
+
+
+-- Task 6
+-- Update BookID 105 price to 650
+
+
+
+-- Task 7
+-- Delete BookID 102
+
+
+
+-- Task 8
+-- Display final Library table
